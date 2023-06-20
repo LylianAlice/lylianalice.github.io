da5314faf3abf0c2dee215a99ea2a7debf834936
LylianAlice <khanhlinhthinhtran@gmail.com>
2023-06-20 11:38:26 +0700

    Final Version

--- assets/js/login.js
+++ assets/js/login.js
@@ -11,19 +11,19 @@ function validate() {
 
 	}
 
-	else if (username == "danghongtham" && password == "208113") {
+	else if (username == "danghongtham" && CryptoJS.MD5(password) == "d78a57ccfbd3f47ec123ead8351429e3") {
 		myPopupT.style.display = "block";
 		myPopupT.addEventListener("click", () => {
 			myPopupT.style.display = "none";
-			window.location.href = "dh208113.html";
+			window.location.href = "dht.html";
 		});
 	}
 
-	else if (username == "huynhngocnga" && password == "1471") {
+	else if (username == "huynhngocnga" && CryptoJS.MD5(password) == "8b0d268963dd0cfb808aac48a549829f") {
 		myPopupN.style.display = "block";
 		myPopupN.addEventListener("click", () => {
 			myPopupN.style.display = "none";
-			window.location.href = "hn1471.html";
+			window.location.href = "hnn.html";
 		});
 	}
 
